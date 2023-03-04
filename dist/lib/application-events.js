ee09f6c8e9614dee1a7f8e95d0729e15a5af382c
Benjamin Russell <benrr101@outlook.com>
2023-03-03 23:37:10 -0600

    "Publish" build for internal consumption

---
+++ dist/lib/application-events.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ApplicationEvents = void 0;
+exports.ApplicationEvents = {
+    BOOTSTRAP_END: "bootstrapEnd",
+    VALIDATE_PROJECT: "validateProject",
+};
+//# sourceMappingURL=application-events.js.map
